cf671be05f38f632d719c5dc0ffe5fec5e65beb5
Joseph Tatum <josephtatum@gmail.com>
2019-12-02 17:22:12 -0800

    fixed function

--- challenges/arrayReverse/__tests__/arrayReverse.test.js
+++ challenges/arrayReverse/__tests__/arrayReverse.test.js
@@ -4,6 +4,7 @@ describe('validator module', () => {
     describe('01 validation', () => {
         it('reverses an array that is passed into it', () => {
             expect(reverseArray([1, 2, 3])).toEqual([3, 2, 1]);
+            expect(reverseArray([1, 2, 3])).toEqual([3, 2, 1]);
         });
     });
 });
